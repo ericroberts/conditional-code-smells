c4d07c8b8487f6bbf0e33b16bb9869bf4e52bc34
Eric Roberts <ericroberts@gmail.com>
2014-08-13 20:09:23 -0400

    Remove stuff that is not relevant (maybe shouldn't blindly copy/paste from other projects)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -19,7 +19,6 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
-  config.include Devise::TestHelpers, type: :controller
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
@@ -43,9 +42,4 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
-
-  config.before do
-    r = Redis.new
-    r.keys.each { |k| r.del k }
-  end
 end
